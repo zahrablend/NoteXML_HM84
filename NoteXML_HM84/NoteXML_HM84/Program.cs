c7e9f93415ca59cdfe03e91d7f889675007adeef
zahrablend <102861443+zahrablend@users.noreply.github.com>
2022-10-17 15:44:27 +0300

    Add files via upload

---
+++ NoteXML_HM84/NoteXML_HM84/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml;
+
+namespace NoteXML_HM84
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets create a note!");
+            Console.Write("to: ");
+            var to = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("from: ");
+            var from = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("heading: ");
+            var heading = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("body: ");
+            var body = Console.ReadLine();
+
+            Console.WriteLine("Your note is saved!");
+
+
+            CreatingNote(to, from, heading, body);
+        }
+
+        private static void CreatingNote(string? to, string? from, string? heading, string? body)
+        {
+            XmlWriter writer = XmlWriter.Create("note.xml");
+
+            writer.WriteStartDocument();
+            writer.WriteStartElement("note");
+
+            writer.WriteStartElement("to");
+            writer.WriteString(to);
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("from");
+            writer.WriteString(from);
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("heading");
+            writer.WriteString(heading);
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("body");
+            writer.WriteString(body);
+            writer.WriteEndElement();
+
+            writer.WriteEndDocument();
+            writer.Flush();
+        }
+    }
+}
